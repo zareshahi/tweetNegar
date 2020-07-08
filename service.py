5fef6014898d85d912722b20a253fa8a0214dc31
pooshesh <mohammad.pooshesh@gmail.com>
2020-07-09 00:40:23 +0430

    change sevicve file

--- service.py
+++ service.py
@@ -12,10 +12,15 @@ def bop(bot, update):
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
+def hello(update, context):
+    update.message.reply_text(
+        'سلام علیکم {}'.format(update.message.from_user.first_name))
+
 def main():
     updater = Updater('1130555713:AAEfpdkRQIT7AbvjyUQfWQMZN2pzRRVwf0w')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('hello', hello))
     updater.start_polling()
     updater.idle()
 
