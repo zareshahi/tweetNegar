0e527368633e2f63e328870dc5011f2c16099044
Ali Zareshahi <a.zareshahi1377@gmail.com>
2020-07-09 11:48:30 +0430

    docs(app): code cleaning

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from io import BytesIO
 
 import arabic_reshaper
-import numpy as np
 from bidi.algorithm import get_display
+from numpy import array, uint8, zeros
 from PIL import Image, ImageDraw, ImageFont
 
 # Make canvas and set the color
-img = np.zeros((1920, 1080, 3), np.uint8)
+# TODO: use flexible size by user input
+img = numpy.zeros((1920, 1080, 3), numpy.uint8)
 b, g, r, a = 0, 255, 0, 0
 
 # Initial path's
+# TODO: use config.json and import more fonts
 vazir_font_path = './assets/font/vazir/Vazir-Light.ttf'
 instagram_story_path = './assets/images/output/insta_storty.jpeg'
 
 # firts you must prepare your text (you dont need this for english text)
+# FIXME: get text from user by get_text() function
 text = "بسم الله الرحمان الرحیم"
+# TODO: change text size by text length
 reshaped_text = arabic_reshaper.reshape(text)    # correct its shape
 bidi_text = get_display(reshaped_text)           # correct its direction
 
@@ -30,9 +34,7 @@ draw = ImageDraw.Draw(img_pil)
 w, h = draw.textsize(text.encode('utf8'))
 draw.text(((img.shape[1]-w)/2, (img.shape[0]-h)/2),
           bidi_text, font=vazir_font, fill=(b, g, r, a))
-img = np.array(img_pil)
-
-img_pil.show()
+img = numpy.array(img_pil)
 
 
 def get_image(id):
