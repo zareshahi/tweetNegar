ba41683a20841cb5a0cdc5aead86ccf428e189e2
Ali Zareshahi <a.zareshahi1377@gmail.com>
2020-07-14 20:17:07 +0430

    improvement(twitter): use twitter official api instead tweepy library
    fix #9

--- twitter.py
+++ twitter.py
@@ -1,15 +1,41 @@
-import json
-
-import tweepy
+from requests_oauthlib import OAuth1Session
 
 
 class Twitter():
-    def init(self, id):
-        # tweet = [{
-        #     text: self.get_text(id),
-        #     author: self.get_author
-        # }]
-        pass
+    api = ''
+
+    def __init__(self, id):
+        self.__authorization()
+
+    def __authorization(self):
+        ''' authorize connection to twitter api 
+        '''
+        import json
+        # use config json file to hide security API keys
+        # config.json is git ignored - you can see this file template in
+        # config.template.json
+        with open('./config.json') as json_file:
+            config_json = json.load(json_file)
+        # initial config keys from config.json file
+        consumer_key = config_json['twitter']['api_key']
+        consumer_secret = config_json['twitter']['api_key_secret']
+        access_token = config_json['twitter']['access_token']
+        access_token_secret = config_json['twitter']['access_token_secret']
+
+        try:
+            params = {"ids": "1282931844716126208", "format": "detailed"}
+            # Make the request
+            api = OAuth1Session(consumer_key,
+                                client_secret=consumer_secret,
+                                resource_owner_key=access_token,
+                                resource_owner_secret=access_token_secret)
+            response = api.get(
+                "https://api.twitter.com/labs/2/tweets?", params=params)
+            print("Response status: %s" % response.status_code)
+            print("Body: %s" % response.text)
+
+        except:
+            print('some authorization error')
 
     def get_text(self, tweet_id):
         ''' get tweet long text by tweet id
@@ -32,7 +58,7 @@ class Twitter():
             author_detail = {
                 'id': author.id,
                 'name': author.name,
-                'user_name': author.secret_name,
+                'username': author.screen_name,
                 'profile_image': author.profile_image_url
             }
             return author_detail
@@ -46,23 +72,11 @@ class Twitter():
             return ''
 
 
-# use config json file to hide security API keys
-# config.json is git ignored - you can see this file template in
-# config.template.json
-with open('./config.json') as json_file:
-    config_json = json.load(json_file)
-# initial config keys from config.json file
-consumer_key = config_json['twitter']['api_key']
-consumer_secret = config_json['twitter']['api_key_secret']
-access_token = config_json['twitter']['access_token']
-access_token_secret = config_json['twitter']['access_token_secret']
-# use tweepy to authorization twitter api
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# # use tweepy to authorization twitter api
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
-
-twitter = Twitter()
+# api = tweepy.API(auth)
 
 samples = {
     'with_photo': {
@@ -86,7 +100,7 @@ samples = {
         'url': 'https://twitter.com/pariis_tar/status/1282732786688958464?s=20'
     },
     'removed': {
-        'id': 1282927709795090432,
+        'id': 1283001644892909568,
         'url': 'https://twitter.com/moeb_ir/status/1282927709795090432'
     },
     'video': {
@@ -97,19 +111,23 @@ samples = {
         'id': 1279989094349750272,
         'url': 'https://twitter.com/MortezaJaliliIR/status/1279989094349750272?s=20'
     }
-
 }
 
+twitter = Twitter(id=1279989094349750272)
+
 # get tweet by list of id
-public_tweets = api.statuses_lookup([1281841440570650625, 1281969401420554241])
-file = open('tweet.txt', 'w')
-for tweet in samples:
-    tid = samples[tweet]['id']
-    file.write(twitter.get_text(tid))
-    file.writelines('\n')
-    file.writelines('\n')
-    file.write(twitter.get_author(tid))
-    file.writelines('\n')
-    file.write('==========================================================')
-    file.writelines('\n')
-file.close()
+# # public_tweets = api.statuses_lookup([1281841440570650625, 1281969401420554241])
+# try:
+#     file = open('tweet.txt', 'w')
+#     for tweet in samples:
+#         tid = samples[tweet]['id']
+#         file.write(twitter.get_text(tid))
+#         file.writelines('\n')
+#         file.writelines('\n')
+#         file.write(twitter.get_author(tid)['name'])
+#         file.writelines('\n')
+#         file.write('==========================================================')
+#         file.writelines('\n')
+#     file.close()
+# except expression as identifier:
+#     pass
